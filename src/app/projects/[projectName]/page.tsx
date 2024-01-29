23d25575d163e62acf3c9f0aeb6460fed6dab7f3
Sunnie Kapar <kaparsunnie@gmail.com>
2024-01-28 19:53:46 -0500

    .

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -3,21 +3,10 @@ import { notFound } from 'next/navigation';
 import Test from './Test';
 
 export function generateStaticParams() {
-  return projectData.map((project) => {
-    return {
-      params: {
-        projectName: project.projectPage,
-      },
-    };
-  });
+   return [{ projectName: '1' }, { projectName: '2' }, { projectName: '3' }]
 }
 
 export default function Page({ params }: { params: { projectName: string } }) {
-  const allPaths = generateStaticParams();
-  const isValidPath = allPaths.some(
-    (path) => path.params.projectName === params.projectName
-  );
-  if (!isValidPath) notFound();
   const project = projectData.find((p) => params.projectName === p.projectPage);
   return (
     <Test {...project!} />
