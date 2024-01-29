0846a7132baa4b125541bf506c7c50078958f735
Sunnie Kapar <kaparsunnie@gmail.com>
2024-01-28 19:22:28 -0500

    did this in two sittings 😎 (nope)

--- src/app/_layouts/Details/Details.tsx
+++ src/app/_layouts/Details/Details.tsx
@@ -15,15 +15,15 @@ import {
 
 export default function Details() {
   const icons = [
-    <SiMongodb />,
-    <SiExpress />,
-    <SiReact />,
-    <SiNodedotjs />,
-    <SiExpo />,
-    <SiNextdotjs />,
-    <SiFirebase />,
-    <SiTypescript />,
-    <SiTailwindcss />,
+    <SiMongodb key="SiMongodb" />,
+    <SiExpress key="SiExpress" />,
+    <SiReact key="SiReact" />,
+    <SiNodedotjs key="SiNodedotjs" />,
+    <SiExpo key="SiExpo" />,
+    <SiNextdotjs key="SiNextdotjs" />,
+    <SiFirebase key="SiFirebase" />,
+    <SiTypescript key="SiTypescript" />,
+    <SiTailwindcss key="SiTailwindcss" />,
   ];
   return (
     <FadeIn list delay={0.5} className="grid xl:grid-cols-seven-ten mt-4 gap-4">
@@ -43,9 +43,13 @@ export default function Details() {
           <p className="text-2xl sm:text-3xl font-semibold leading-tight mb-4 ">
             My Tech Stack.
           </p>
-          <FadeIn list delay={0.15} className="flex flex-wrap gap-6 text-3xl md:text-4xl mb-8 justify-center md:items-start">
+          <FadeIn
+            list
+            delay={0.15}
+            className="flex flex-wrap gap-6 text-3xl md:text-4xl mb-8 justify-center md:items-start"
+          >
             {icons.map((icon, index) => {
-              return <FadeInChild key={index + 100}>{icon}</FadeInChild>;
+              return <FadeInChild key={index}>{icon}</FadeInChild>;
             })}
           </FadeIn>
           <p className="text-sm opacity-80">
