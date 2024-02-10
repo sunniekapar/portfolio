160c69dd8fa57ffd1f0bcb4cf0579a1bef72d60b
Sunnie Kapar <kaparsunnie@gmail.com>
2024-02-10 10:55:23 -0500

    Default theme change

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,16 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
-
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
