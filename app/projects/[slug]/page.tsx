30f045949222944b9c8e17ed22b83f6b1d1fd78b
Sunnie Kapar <kaparsunnie@gmail.com>
2024-06-20 14:18:07 -0400

    project image hotfix

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -56,7 +56,7 @@ export default function Page({ params }: { params: { slug: string } }) {
   const cleanTitle = clean(title);
   return (
     <>
-      <div className="animate-background ease-smooth pointer-events-none absolute left-0 top-0 h-full w-full overflow-clip opacity-20 duration-1000">
+      <div className="animate-background ease-smooth pointer-events-none absolute left-0 top-0 h-full w-full overflow-clip opacity-15 duration-1000">
         <div className="absolute top-0 h-1/4 w-full bg-[radial-gradient(ellipse_at_top,hsl(var(--accent)),transparent,transparent)] blur-3xl" />
         <div className="absolute -left-20 h-full w-1/4 bg-[radial-gradient(ellipse_at_left,hsl(var(--accent)),transparent,transparent)] blur-3xl" />
         <div className="absolute -right-20 bottom-0 h-3/4 w-5/6 bg-[radial-gradient(ellipse_at_bottom_right,hsl(var(--accent)),transparent,transparent)] blur-3xl" />
@@ -98,7 +98,7 @@ export default function Page({ params }: { params: { slug: string } }) {
           <NavigationMenu buttons={navButtons} />
         </FadeIn>
 
-        <FadeIn className="relative aspect-[4/3] w-full max-w-[600px] place-self-center rounded-lg">
+        <div className="animate-appear duration-1000 ease-smooth relative aspect-[4/3] w-full max-w-[600px] place-self-center rounded-lg">
           <div className="absolute -inset-0.5 h-full w-full animate-tilt rounded-lg bg-gradient-to-tr from-accent to-accent-foreground opacity-100 blur-xl" />
           <Image
             src={`/images/projects/${cleanTitle}.jpg`}
@@ -106,7 +106,7 @@ export default function Page({ params }: { params: { slug: string } }) {
             fill
             className="pointer-events-none relative rounded-lg object-cover"
           />
-        </FadeIn>
+        </div>
       </Template>
     </>
   );
